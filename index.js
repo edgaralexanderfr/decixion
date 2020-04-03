7351dfb39ab291f3d748cbbb71ef560402016d18
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2020-04-03 13:47:58 -0500

    Define engine file as the main file in package.js

--- index.js
+++
@@ -1,4 +0,0 @@
-const decixion = require('./src/decixion');
-const game = require('./src/game');
-
-decixion.init(game);
---
+++ tests/test.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const decixion = require('../src/decixion');
+const game = require('../src/game');
+
+decixion.init(game);
